946e7d25a445614eb79025117fd3fcf79ced7c68
Emery Kouassi Assogba <assogba.emery@gmail.com>
2021-07-08 07:54:00 +0100

    add exercice with three hosts and three routers

---
+++ ipmininet_inginious_exercices/exercices/lanwith3routers.py
@@ -0,0 +1,134 @@
+
+import ipmininet
+import sys
+import gettext
+from ipmininet.cli import IPCLI
+from ipmininet.ipnet import IPNet
+from ipmininet.router.config import RouterConfig
+from ipmininet.iptopo import IPTopo
+
+
+from mininet.log import lg
+
+from inginious.grader import Grader
+
+"""
+fr = gettext.translation('base', localedir='locales', languages=['fr'])
+fr.install()
+_ = fr.gettext # french
+
+"""
+
+_ = gettext.gettext
+
+"""
+
+This file contains a simple topology with three routers and three hosts
+      p1   p4   p2
+  h1 ---r1--r2---- h2
+        p6\  / p5
+           r3
+           |p3
+           h3 
+In this topology, h1, h2, h3 and r1, r2, r3 have respectively no IP address. 
+You have to  give an IP address to each host interface and the routers interfaces 
+to make them communicate based to subnet address p1, p2, p3, p4, p5 
+prefix length n and address family fa.
+
+
+"""
+        
+
+class LANWith3Routers(IPTopo):
+
+    def __init__(self, family, subnet=[], prefixlen=[], gw=[], *args, **kwargs):
+        self.family = family
+        self.subnet = subnet
+        self.prefixlen = prefixlen
+        self.gw = gw
+        super().__init__(*args, **kwargs)
+
+    def build(self, *args, **kwargs):
+        """
+        """
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        r1 = self.addRouter('r1', config=(RouterConfig))
+        r2 = self.addRouter('r2', config=(RouterConfig))
+        r3 = self.addRouter('r3', config=(RouterConfig))
+        self.addLink(r1, h1)
+        self.addLink(r2, h2)
+        self.addLink(r3, h3)
+        self.addLink(r1, r2)
+        self.addLink(r2, r3)
+        self.addLink(r1, r3)
+        super(LANWith3Routers, self).build(*args, **kwargs)
+        
+    def pre_stop(self, net):
+        test = Grader(net)
+        nodes = ["h1", "r1"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[0], self.prefixlen[0], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        nodes = ["h2", "r2"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[1], self.prefixlen[1], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        nodes = ["h3", "r3"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[2], self.prefixlen[2], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        
+        nodes = ["r1", "r2"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[3], self.prefixlen[3], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        
+        nodes = ["r2", "r3"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[4], self.prefixlen[4], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        
+        nodes = ["r1", "r3"]
+        state, feedback = test.checksubnet_addr(self.family, self.subnet[5], self.prefixlen[5], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        nodes = ["h1"]
+        state, feedback = test.check_default_route(self.family, self.gw[0], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        nodes = ["h2"]
+        state, feedback = test.check_default_route(self.family, self.gw[1], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        
+        nodes = ["h3"]
+        state, feedback = test.check_default_route(self.family, self.gw[2], nodes)
+        print(state, file=open("state3.txt", "a"))
+        print(feedback, file=open("feedback3.txt", "a"))
+        
+        super(LANWith3Routers, self).post_build(net)
+
+
+ipmininet.DEBUG_FLAG = True
+lg.setLogLevel("info")
+
+"""
+net = IPNet(topo=TwoPCLANWithRouter("4", "192.168.1.0", "24", "192.168.2.0", "24"), use_v4=False, use_v6=False, allocate_IPs=False)
+
+try:
+    net.start()
+    IPCLI(net)
+finally:
+    net.stop()
+    
+"""
---
+++ ipmininet_inginious_exercices/tests/test_lanwith3routers.py
@@ -0,0 +1,212 @@
+"""This module tests twopclan.py host ip configuration exercice """
+import pytest
+import time 
+
+from ipmininet.clean import cleanup
+from . import require_root
+from exercices.lanwith3routers import LANWith3Routers
+from ipmininet.ipnet import IPNet
+from ipmininet.iptopo import IPTopo
+
+
+@require_root
+def test_LANWith3Routers():
+    subnet=["192.168.1.0", "192.168.2.0", "192.168.3.0", "192.168.4.0", \
+             "192.168.5.0", "192.168.6.0"]
+    prefixlen=["24", "24", "24", "24", "24", "24"]
+    gw=["192.168.1.1", "192.168.2.1", "192.168.3.1"]
+    try:
+        net = IPNet(topo=LANWith3Routers("4", subnet, prefixlen, gw) \
+                 , use_v4=False, use_v6=False, allocate_IPs=False)
+        net.start()
+        
+        itf1 = net["h1"].intf("h1-eth0")
+        itf1.ip = "192.168.1.2/24"
+        itf1.prefixLen == 24
+        
+        
+        
+        itf2 = net["r1"].intf("r1-eth0")
+        itf2.ip = "192.168.1.1/24"
+        itf2.prefixLen == 24
+        
+        net["h1"].cmd("ip route add default via 192.168.1.1")
+        
+        
+        itf3 = net["h2"].intf("h2-eth0")
+        itf3.ip = "192.168.2.2/24"
+        itf3.prefixLen == 24
+        
+        
+        itf4 = net["r2"].intf("r2-eth0")
+        itf4.ip = "192.168.2.1/24"
+        itf4.prefixLen == 24
+        
+        net["h2"].cmd("ip route add default via 192.168.2.1")
+        
+        
+        
+        itf5 = net["h3"].intf("h3-eth0")
+        itf5.ip = "192.168.3.2/24"
+        itf5.prefixLen == 24
+        
+        
+        itf6 = net["r3"].intf("r3-eth0")
+        itf6.ip = "192.168.3.1/24"
+        itf6.prefixLen == 24
+        
+        net["h3"].cmd("ip route add default via 192.168.3.1")
+        
+        
+        itf7 = net["r1"].intf("r1-eth1")
+        itf7.ip = "192.168.4.1/24"
+        itf7.prefixLen == 24
+        
+        itf8 = net["r2"].intf("r2-eth1")
+        itf8.ip = "192.168.4.2/24"
+        itf8.prefixLen == 24
+        
+        itf9 = net["r2"].intf("r2-eth2")
+        itf9.ip = "192.168.5.1/24"
+        itf9.prefixLen == 24
+        
+        itf10 = net["r3"].intf("r3-eth1")
+        itf10.ip = "192.168.5.2/24"
+        itf10.prefixLen == 24
+        
+        itf11 = net["r1"].intf("r1-eth2")
+        itf11.ip = "192.168.6.1/24"
+        itf11.prefixLen == 24
+        
+        itf10 = net["r3"].intf("r3-eth2")
+        itf10.ip = "192.168.6.2/24"
+        itf10.prefixLen == 24
+         
+        net.stop()
+        with open('state3.txt', 'r') as f:
+            state = f.read().replace('\n', '')
+        f.close()
+        assert "Failed" not in state
+    finally:
+        cleanup()
+        
+"""
+@require_root
+def test_TwoPCLANWithRouter_IPv6():
+    try:
+        net = IPNet(topo=TwoPCLANWithRouter("6", "fc00:0:1::", "48", \
+                  "fc00:0:1::1", "fc00:0:2::", "48", "fc00:0:2::1")  \
+                  , use_v4=False, use_v6=False, allocate_IPs=False)
+        net.start()
+        
+        itf1 = net["h1"].intf("h1-eth0")
+        itf1.ip = "fc00:0:1::2/48"
+        itf1.prefixLen == 48
+        
+        
+        itf2 = net["r"].intf("r-eth0")
+        itf2.ip = "fc00:0:1::1/48"
+        itf2.prefixLen == 48
+        
+        r = net["h1"].cmd("ip -6 route add default via fc00:0:1::1")
+        
+        itf3 = net["h2"].intf("h2-eth0")
+        itf3.ip = "fc00:0:2::2/48"
+        itf3.prefixLen == 48
+        
+        
+        itf4 = net["r"].intf("r-eth1")
+        itf4.ip = "fc00:0:2::1/48"
+        itf4.prefixLen == 48
+        
+        r = net["h2"].cmd("ip -6 route add default via fc00:0:2::1")
+        
+        net.stop()
+        with open('state2.txt', 'r') as f:
+            state = f.read().replace('\n', '')
+        f.close()
+        assert "Failed" not in state
+    finally:
+        cleanup()
+
+
+@require_root
+def test_TwoPCLANWithRouter_IPv4_wrong_gw():
+    try:
+        net = IPNet(topo=TwoPCLANWithRouter("4", "192.168.1.0", "24", \
+                  "192.168.1.1", "192.168.2.0", "24", "192.168.2.1")  \
+                 , use_v4=False, use_v6=False, allocate_IPs=False)
+        net.start()
+        
+        itf1 = net["h1"].intf("h1-eth0")
+        itf1.ip = "192.168.1.2/24"
+        itf1.prefixLen == 24
+        
+        
+        
+        itf2 = net["r"].intf("r-eth0")
+        itf2.ip = "192.168.1.1/24"
+        itf2.prefixLen == 24
+        
+        r = net["h1"].cmd("ip route add default via 192.168.2.1")
+        
+        
+        itf3 = net["h2"].intf("h2-eth0")
+        itf3.ip = "192.168.2.2/24"
+        itf3.prefixLen == 24
+        
+        
+        itf4 = net["r"].intf("r-eth1")
+        itf4.ip = "192.168.2.1/24"
+        itf4.prefixLen == 24
+        
+        r = net["h2"].cmd("ip route add default via 192.168.1.1")
+         
+        net.stop()
+        with open('state2.txt', 'r') as f:
+            state = f.read().replace('\n', '')
+        f.close()
+        assert "Failed" in state
+    finally:
+        cleanup()
+        
+        
+@require_root
+def test_TwoPCLANWithRouter_IPv6_wrong_gw():
+    try:
+        net = IPNet(topo=TwoPCLANWithRouter("6", "fc00:0:1::", "48", \
+                  "fc00:0:1::1", "fc00:0:2::", "48", "fc00:0:2::1")  \
+                  , use_v4=False, use_v6=False, allocate_IPs=False)
+        net.start()
+        
+        itf1 = net["h1"].intf("h1-eth0")
+        itf1.ip = "fc00:0:1::2/48"
+        itf1.prefixLen == 48
+        
+        
+        itf2 = net["r"].intf("r-eth0")
+        itf2.ip = "fc00:0:1::1/48"
+        itf2.prefixLen == 48
+        
+        r = net["h1"].cmd("ip -6 route add default via fc00:0:2::1")
+        
+        itf3 = net["h2"].intf("h2-eth0")
+        itf3.ip = "fc00:0:2::2/48"
+        itf3.prefixLen == 48
+        
+        
+        itf4 = net["r"].intf("r-eth1")
+        itf4.ip = "fc00:0:2::1/48"
+        itf4.prefixLen == 48
+        
+        r = net["h2"].cmd("ip -6 route add default via fc00:0:1::1")
+        
+        net.stop()
+        with open('state2.txt', 'r') as f:
+            state = f.read().replace('\n', '')
+        f.close()
+        assert "Failed" in state
+    finally:
+        cleanup()
+        
+"""
